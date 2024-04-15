0d1bc848a563368d97591864148817bf244e24c3
Bryan Bradley <bryrbradley@gmail.com>
2024-04-14 22:21:18 -0600

    Final version, CRUD complete, README complete, JUnit test complete.

--- BootSnack/src/test/java/com/skilldistillery/snack/BootSnackApplicationTests.java
+++ BootSnack/src/test/java/com/skilldistillery/snack/BootSnackApplicationTests.java
@@ -1,13 +1,46 @@
 package com.skilldistillery.snack;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-@SpringBootTest
-class BootSnackApplicationTests {
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.skilldistillery.snack.controllers.SnackController;
+import com.skilldistillery.snack.data.SnackDAO;
+import com.skilldistillery.snack.entities.Snack;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(SnackController.class)
+public class BootSnackApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private SnackDAO snackDAO;
+
+//	JUnit tests for controller
 
 	@Test
-	void contextLoads() {
+	public void testGetSnack() throws Exception {
+		// Mock the behavior of the DAO layer
+		Snack mockSnack = new Snack();
+		mockSnack.setId(1);
+		mockSnack.setName("Test Snack");
+		when(snackDAO.findById(1)).thenReturn(mockSnack);
+
+		// Perform the GET request and verify the response
+		mockMvc.perform(get("/getSnack.do?id=1")).andExpect(status().isOk()).andExpect(view().name("views/show"))
+				.andExpect(model().attribute("snack", mockSnack));
 	}
 
 }
